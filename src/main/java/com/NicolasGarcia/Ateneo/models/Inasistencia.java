c99b1d854712dc3c828f853291501c920a99d5b4
NicoGarcia12 <nicolasgarcia9812@hotmail.com>
2024-02-22 02:51:25 -0300

    Igualdad de generación de ID

--- src/main/java/com/NicolasGarcia/Ateneo/models/Inasistencia.java
+++ src/main/java/com/NicolasGarcia/Ateneo/models/Inasistencia.java
@@ -18,7 +18,7 @@ import lombok.NoArgsConstructor;
 public class Inasistencia {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 
 	@ManyToOne(fetch = FetchType.LAZY)
