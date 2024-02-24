5fc40deccc1bca3b560e43fb8c3acda6d545cb7e
NicoGarcia12 <nicolasgarcia9812@hotmail.com>
2024-02-24 20:29:50 -0300

    Repositorios de Profesor y Materia

---
+++ src/main/java/com/NicolasGarcia/Ateneo/repositories/MateriaRepository.java
@@ -0,0 +1,9 @@
+package com.NicolasGarcia.Ateneo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.NicolasGarcia.Ateneo.models.Materia;
+
+public interface MateriaRepository extends JpaRepository<Materia, Long>{
+
+}
---
+++ src/main/java/com/NicolasGarcia/Ateneo/repositories/ProfesorRepository.java
@@ -0,0 +1,9 @@
+package com.NicolasGarcia.Ateneo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.NicolasGarcia.Ateneo.models.Profesor;
+
+public interface ProfesorRepository extends JpaRepository<Profesor, Long>{
+
+}
