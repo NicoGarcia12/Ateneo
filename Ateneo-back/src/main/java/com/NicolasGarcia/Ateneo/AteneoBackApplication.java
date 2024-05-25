fe30a791394b9bc5854e449fb0d8dd1444153482
NicoGarcia12 <nicolasgarcia9812@hotmail.com>
2024-05-25 19:56:38 -0300

    Front y back bases

---
+++ Ateneo-back/src/main/java/com/NicolasGarcia/Ateneo/AteneoBackApplication.java
@@ -0,0 +1,13 @@
+package com.NicolasGarcia.Ateneo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AteneoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AteneoBackApplication.class, args);
+	}
+
+}
---
+++ Ateneo-back/src/test/java/com/NicolasGarcia/Ateneo/AteneoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NicolasGarcia.Ateneo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AteneoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
