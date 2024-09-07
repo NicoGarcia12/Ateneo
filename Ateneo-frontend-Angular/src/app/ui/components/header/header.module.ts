2a98d99e6680c472a20e601b66a8d6a1ebc90802
NicoGarcia12 <nicolasgarcia9812@hotmail.com>
2024-09-07 18:02:13 -0300

    Header y clases de los botones

--- Ateneo-frontend-Angular/src/app/ui/components/header/header.module.ts
+++ Ateneo-frontend-Angular/src/app/ui/components/header/header.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { HeaderComponent } from './header.component';
-import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule, MatButtonModule],
+  imports: [MatButtonModule, CommonModule],
   exports: [HeaderComponent],
 })
 export class HeaderModule {}
