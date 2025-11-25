4773cce9c24344c41dba5e87f9e172fdb9696acd
Nicolás García <nicolasgarcia9812@hotmail.com>
2025-11-24 21:47:29 -0300

    Prettier back

--- Ateneo-backend-Express/src/config/prisma.ts
+++ Ateneo-backend-Express/src/config/prisma.ts
@@ -2,5 +2,5 @@ import { PrismaClient } from '@prisma/client';
 import config from '../../prisma/prisma.config';
 
 export const prisma = new PrismaClient({
-	datasourceUrl: config.datasource.url,
+    datasourceUrl: config.datasource.url
 });
